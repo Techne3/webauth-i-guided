e7101ce518e2ec1f9962ada7e30cf1aeca901b54
Timothy Sharkey <Techne3@gmail.com>
2019-11-11 10:36:00 -0700

    init commit

--- api/api-router.js
+++ api/api-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const bcrypt = require('bcryptjs')
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
@@ -10,4 +11,20 @@ router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
 
+router.post('/hash', (req,res)=> {
+  //read a password from the body 
+  //hash the password 
+  //return it to the user in an object that looks like {password: 'original password', hash: 'hashed password'}
+  let { username, password } = req.body;
+
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14)
+  credentials.password = hash;
+  
+  if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+    return res.status(401).json({ error: 'Incorrect credentials' });
+  }
+
+
+})
 module.exports = router;
